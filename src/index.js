f19f903ae47391fca43e38826b00f3118a1fe2bc
Guilherme Gomes <106498363+GuilhermeGomesti1@users.noreply.github.com>
2023-12-06 13:32:00 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+const Film = mongoose.model("Film", {
+  title: String,
+  description: String,
+  image_url: String,
+  trailer_url: String,
+});
+
+app.get("/", async (req, res) => {
+  const films = await Film.find();
+  return res.send(films);
+});
+
+app.delete("/:id", async (req, res) => {
+  const film = await Film.findByIdAndDelete(req.params.id);
+  return res.send(film);
+});
+
+app.put("/:id", async (req, res) => {
+  const film = await Film.findByIdAndUpdate(req.params.id, {
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.trailer_url,
+  }, {new: true});
+  return res.send(film);
+});a
+
+app.post("/", async (req, res) => {
+  const film = new Film({
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.trailer_url,
+  });
+  await film.save();
+  return res.send(film);
+});
+
+app.listen(PORT, () => {
+  mongoose.connect(
+    "mongodb+srv://guilhermegomesti1:W0xosSivwLNCZ6Vv@starwars-api.h1gzmul.mongodb.net/?retryWrites=true&w=majority"
+  );
+
+  console.log("App running");
+});
